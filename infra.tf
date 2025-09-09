01e4d04d4d91d70fd6254758b124b5a8149211eb
Gabriel Pampolha <pampolhag@gmail.com>
2025-09-09 20:10:53 -0300

    feat: defined generateScenario schedule

--- infra.tf
+++ infra.tf
@@ -32,6 +32,11 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+resource "aws_iam_role_policy_attachment" "lambda_dynamodb" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess_v2"
+}
+
 resource "aws_lambda_function" "generator" {
   filename         = "assert-generator-payload.zip"
   function_name    = "assert-generator"
@@ -45,6 +50,53 @@ resource "aws_lambda_function" "generator" {
     variables = {
       OPENROUTER_API_KEY = var.openrouter_api_key
     }
+  }
+}
+
+resource "aws_iam_role" "scheduler_exec" {
+  name = "scheduler_exec_role"
+  assume_role_policy = jsonencode({
+    Version: "2012-10-17",
+    Statement: [
+        {
+            Effect: "Allow",
+            Principal: {
+                Service: "scheduler.amazonaws.com"
+            },
+            Action: "sts:AssumeRole"
+        },
+        {
+            Action: [
+                "lambda:InvokeFunction"
+            ],
+            Effect: "Allow",
+            Resource: aws_lambda_function.generator.arn
+        }
+    ]
+  })
+}
+
+resource "aws_scheduler_schedule" "generate_scenario" {
+  name       = "generate-scenario-schedule"
+  group_name = "default"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  schedule_expression = "rate(24 hours)"
+
+  target {
+    arn      = aws_lambda_function.generator.arn
+    role_arn = aws_iam_role.scheduler_exec.arn
+
+    input = jsonencode({
+      FunctionName: aws_lambda_function.generator.arn,
+      InvocationType: "event",
+      Payload: jsonencode({
+        action: "generateScenario"
+      })
+    })
   }
 }
 
