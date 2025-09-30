90d3c8b85fa4127a64c31a1f3dd828082bb38940
Gabriel Pampolha <pampolhag@gmail.com>
2025-09-30 15:49:07 -0300

    fix: cw agent config file is put after install

--- deploy.sh
+++ deploy.sh
@@ -22,15 +22,15 @@ echo "put ./bin/assert $BIN_TMP_REMOTE_PATH" | sftp -i $SSH_KEY $SSH_LOGIN@$IP
 echo "put ./cw-agent-config.json $CW_CONFIG_TMP_REMOTE_PATH" | sftp -i $SSH_KEY $SSH_LOGIN@$IP
 
 BIN_REMOTE_PATH=/usr/local/bin/assert
-CW_CONFIG_REMOTE_PATH=/opt/aws/amazon-cloudwatch-agent/bin/config.json
 LOG_REMOTE_PATH=/var/log/assert.log
 
 ssh -i $SSH_KEY $SSH_LOGIN@$IP "sudo mv $BIN_TMP_REMOTE_PATH $BIN_REMOTE_PATH && sudo chmod +x $BIN_REMOTE_PATH"
 ssh -i $SSH_KEY $SSH_LOGIN@$IP "sudo touch $LOG_REMOTE_PATH && sudo chown $SSH_LOGIN:$SSH_LOGIN $LOG_REMOTE_PATH && sudo chmod 644 $LOG_REMOTE_PATH"
-ssh -i $SSH_KEY $SSH_LOGIN@$IP "sudo mv $CW_CONFIG_TMP_REMOTE_PATH $CW_CONFIG_REMOTE_PATH"
 
+CW_CONFIG_REMOTE_PATH=/opt/aws/amazon-cloudwatch-agent/bin/config.json
 CW_BIN_REMOTE_PATH=/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl
-ssh -i $SSH_KEY $SSH_LOGIN@$IP "sudo yum install amazon-cloudwatch-agent"
+ssh -i $SSH_KEY $SSH_LOGIN@$IP "sudo yum install amazon-cloudwatch-agent -y"
+ssh -i $SSH_KEY $SSH_LOGIN@$IP "sudo mv $CW_CONFIG_TMP_REMOTE_PATH $CW_CONFIG_REMOTE_PATH"
 ssh -i $SSH_KEY $SSH_LOGIN@$IP "sudo $CW_BIN_REMOTE_PATH -a fetch-config -m ec2 -s -c file:$CW_CONFIG_REMOTE_PATH"
 
 ssh -i $SSH_KEY $SSH_LOGIN@$IP "$BIN_REMOTE_PATH > $LOG_REMOTE_PATH 2>&1 &"
